8fdd67a83a9e71f365266bc957e90791da7ab351
Felipe de Moraes <felipedemoraes.cursos@gmail.com>
2025-08-13 19:11:59 -0300

    Novos arquivos de aula

---
+++ Curso 2/Modulo I/06_operadoresAritméticos.py
@@ -0,0 +1,39 @@
+'''
+Ordens de Precedência: 
+
+> Parentesis
+> Expoêntes
+> Multiplicações e divisões (da esquerda para a direita)
+> Somas e Subtrações (da esquerda para a direita)
+
+'''
+#%%
+print (10 - 5 * 2)
+
+print((10 - 5) * 2)
+
+print(10 ** 2 * 2)
+
+print(10 ** (2 * 2))
+
+print(10 / 2 * 4)
+
+#%%
+
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+
+#%%
+x = (10 + 5) * 4
+print(x)
+y = 10 / 2 + 25 * 2 - 2 ** 2
+print(y)
+# %%
