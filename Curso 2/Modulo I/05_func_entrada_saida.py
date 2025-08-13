c6fc3464b8404edd15fe3cd564300395f111bb37
Felipe de Moraes <felipedemoraes.cursos@gmail.com>
2025-08-13 18:54:21 -0300

    Novas aulas

---
+++ Curso 2/Modulo I/05_func_entrada_saida.py
@@ -0,0 +1,25 @@
+# Lendo valores com a função input()
+#%%
+nome = input("Informe o seu nome: ")
+#%%
+
+# Exibir dados com a função print()
+
+nome = "Felipe"
+sobrenome = "Moraes"
+
+print(nome, sobrenome)
+
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep="#")
+#%%
+
+nome = input("Informe o seu nome: ")
+idade = input("Informe a sua idade: ")
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#")
+print(nome, idade, sep="#", end="...\n")
+
+#%%
